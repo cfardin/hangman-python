15d36450235b078fb94bfb656b2d244544fc4990
Fardin Chowdhury <cfardin51@gmail.com>
2025-04-29 22:09:46 +0600

    Create hangman_word.py

---
+++ hangman_word.py
@@ -0,0 +1,9 @@
+word_list = [
+    "apple", "cat", "dog", "sun", "tree", "book", "happy", "run", 
+    "house", "bird", "flower", "friend", "water", "school", "smile", 
+    "play", "blue", "music", "green", "pen", "mother", "father", 
+    "bread", "car", "moon", "star", "cold", "hot", "big", "small", 
+    "eat", "drink", "sleep", "door", "window", "chair", "table", 
+    "paper", "red", "yellow", "white", "fish", "egg", "walk", 
+    "time", "day", "night", "year", "month", "teacher"
+]
